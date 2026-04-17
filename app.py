ec100d4c84c93fa13899997e7bb76f195f58846f
Bhuvan Balaji <bhuvanbalaji28@gmail.com>
2026-04-17 20:35:48 +0530

    fixed deployment

--- app.py
+++ app.py
@@ -83,6 +83,4 @@ def dataset_file(filename):
 
 # -------- RUN --------
 if __name__ == "__main__":
-    app.run(debug=True)
-    if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run()
